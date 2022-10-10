a08ca357b82ee0917b6eb0815254aa826134d450
ashkanMa <mobarakabadiashkan@gmail.com>
2022-10-10 15:34:55 +0330

    test

---
+++ test/feature/price/domain/usecase/active_symbols_use_case_test.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:price_tracker/core/helper/exporties.dart';
+
+class MockActiveSymbolsRepository extends Mock
+    implements ActiveSymbolsRepository {}
+
+void main() {
+  late ActiveSymbolsUseCase useCase;
+  late MockActiveSymbolsRepository mockActiveSymbolsRepository;
+  ActiveSymbolsParams params = const ActiveSymbolsParams();
+  List<ActiveSymbol> totalActiveSymbolsList = [];
+  ActiveSymbolsEntity activeSymbols =
+      ActiveSymbolsEntity(msgType: '', activeSymbols: totalActiveSymbolsList);
+  setUp(() {
+    mockActiveSymbolsRepository = MockActiveSymbolsRepository();
+    useCase = ActiveSymbolsUseCase(repository: mockActiveSymbolsRepository);
+  });
+
+  test(
+    'should get active symbols from the repository',
+    () async {
+      // arrange
+      when(() => mockActiveSymbolsRepository.activeSymbols())
+          .thenAnswer((_) async => Right(activeSymbols));
+      // act
+      final result = await useCase(params);
+      // assert
+      expect(result, Right(activeSymbols));
+      verify(() => mockActiveSymbolsRepository.activeSymbols());
+      verifyNoMoreInteractions(mockActiveSymbolsRepository);
+    },
+  );
+}
